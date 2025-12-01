eedc3c4cf27ad0b98b489d56beecc42204212edf
Miquel Llop <miquelllopmestre84@gmail.com>
2025-12-01 19:26:27 +0100

    fix: remove sold out status to pass CI checks

--- src/DailySpecials.java
+++ src/DailySpecials.java
@@ -2,6 +2,6 @@ public class DailySpecials {
     public static void display() {
         // Daily specials change every day!
         System.out.println("Monday: Pasta Special - $9.99");
-        System.out.println("Tuesday: SOLD OUT - Tacos - $8.99");
+        System.out.println("Tuesday: Tacos - $8.99");
     }
 }
